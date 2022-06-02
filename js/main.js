df91592f2b472245060133a31e36665b96e1aee0
MOHAMMED T AGHA <36562531+MohammedTAgha@users.noreply.github.com>
2022-06-02 17:10:46 +0300

    55

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
-  });
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:true,
+    navigation : true,
+     lazyLoad:true,
+    autoHeight:true,
+    autoWidth:true,
+    // navText : ["<i class='fa-thin fa-chevrons-right'></i>","<i class='fa fa-chevron-right'></i>"],
+    navigationText : ['<span class="fa-stack"><i class="fa fa-circle fa-stack-1x"></i><i class="fa fa-chevron-circle-left fa-stack-1x fa-inverse"></i></span>','<span class="fa-stack"><i class="fa fa-circle fa-stack-1x"></i><i class="fa fa-chevron-circle-right fa-stack-1x fa-inverse"></i></span>'],    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
